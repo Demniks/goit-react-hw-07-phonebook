0039be98f938f20c49cd7429ebad86085b15a00b
Демченко Сергей <Demniks@ukr.net>
2023-05-07 22:02:42 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import { fetchContacts } from 'redux/operations';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
+import css from './App.module.css';
 
 const App = () => {
   const contacts = useSelector(getContacts);
@@ -19,7 +20,7 @@ const App = () => {
   const isContacts = Boolean(contacts.length);
 
   return (
-    <div>
+    <div className={css.wrap}>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
